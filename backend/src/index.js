306320ba5bb8af5a09c3564990e5555278ebba09
Rudra Pratap Singh Jadon <160029630+rudrajadon@users.noreply.github.com>
2025-05-27 18:00:07 +0530

    Application Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
