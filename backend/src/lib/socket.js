8edc51d1c8b89b335d0af8c96d6fa9d8d10f19bd
Rudra Pratap Singh Jadon <rudrajadon18@gmail.com>
2025-05-27 12:53:53 +0530

    bug fixed

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -21,7 +21,7 @@ const userSocketMap = {}; // {userId: socketId}
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
-  const userId = socket.handshake.query.userId;
+  const userId = socket.handshake.auth.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
   // io.emit() is used to send events to all the connected clients
